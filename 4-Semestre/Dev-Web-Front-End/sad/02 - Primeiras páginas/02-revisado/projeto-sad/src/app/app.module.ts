5284f92245ff58eb0b6f363024853b4a4569d72f
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-05-12 18:25:22 -0300

    Desenvolvimento do projeto

--- 4-Semestre/Dev-Web-Front-End/sad/02 - Primeiras páginas/02-revisado/projeto-sad/src/app/app.module.ts
+++ 4-Semestre/Dev-Web-Front-End/sad/02 - Primeiras páginas/02-revisado/projeto-sad/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
