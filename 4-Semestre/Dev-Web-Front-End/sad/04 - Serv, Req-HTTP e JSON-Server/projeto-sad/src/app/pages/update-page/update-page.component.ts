973ca567a51266347597348e1adccf0f1d4745f8
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-05-31 17:54:47 -0300

    Commit de desenvolvimento do projeto

--- 4-Semestre/Dev-Web-Front-End/sad/04 - Serv, Req-HTTP e JSON-Server/projeto-sad/src/app/pages/update-page/update-page.component.ts
+++ 4-Semestre/Dev-Web-Front-End/sad/04 - Serv, Req-HTTP e JSON-Server/projeto-sad/src/app/pages/update-page/update-page.component.ts
@@ -7,22 +7,19 @@ import { Users, createUser } from 'src/app/model/users.model';
 @Component({
   selector: 'app-update-page',
   templateUrl: './update-page.component.html',
-  styleUrls: ['./update-page.component.css']
+  styleUrls: ['./update-page.component.css'],
 })
 export class UpdatePageComponent {
-  searchTerm: string = "";
+  searchTerm: string = '';
   searchResult: any;
   // searchResult: Users = createUser();
 
-  constructor(private userService: UserService,
-              private route: Router) {
-    
-  }
+  constructor(private userService: UserService, private route: Router) {}
 
   public searchUser() {
     this.userService.getUserByName(this.searchTerm).subscribe((result) => {
       this.searchResult = result;
-    })
+    });
   }
 
   public update() {
