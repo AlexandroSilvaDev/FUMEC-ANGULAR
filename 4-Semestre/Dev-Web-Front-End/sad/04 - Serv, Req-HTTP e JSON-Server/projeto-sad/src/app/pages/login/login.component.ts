562f16cc9ac1b3bd5ee78de73f31d4b4e4924671
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-06-02 18:02:13 -0300

    Desenvolvimento do projeto

--- 4-Semestre/Dev-Web-Front-End/sad/04 - Serv, Req-HTTP e JSON-Server/projeto-sad/src/app/pages/login/login.component.ts
+++ 4-Semestre/Dev-Web-Front-End/sad/04 - Serv, Req-HTTP e JSON-Server/projeto-sad/src/app/pages/login/login.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from 'src/app/services/auth.service';
 import { Users } from 'src/app/model/users.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  user?: Users;
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor(private authService: AuthService) {
-
+  constructor(
+              private authService: AuthService,
+              private formBuilder: FormBuilder) {
+  
   }
 
-  logIn() {
-    this.authService.logIn(this.user);
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
+  }
+
+  onSubmit() {
+    // if (this.loginForm.invalid) {
+    //   return;
+    // }
+    
+    const email = this.loginForm.controls['email'].value;
+    const password = this.loginForm.controls['password'].value;
+
+    this.authService.logIn( email, password );
+
+    console.log('Tentou logar');
+
+    // .subscribe((response) => {
+    //   if (response.success) {
+    //     console.log("Login bem-sucedido", response.user);
+        
+    //   } else {
+    //     console.log("Credenciais inválidas", response.message);
+    //   }
+    // });
   }
 }
--- 4-Semestre/Dev-Web-Front-End/sad/04 - Serv, Req-HTTP e JSON-Server/projeto-sad/src/app/services/auth.service.ts
+++ 4-Semestre/Dev-Web-Front-End/sad/04 - Serv, Req-HTTP e JSON-Server/projeto-sad/src/app/services/auth.service.ts
@@ -1,45 +1,77 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { EventEmitter, Injectable, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 import { Users } from '../model/users.model';
+import { UserService } from './user.service';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {
-  private canLog: boolean = false;
+export class AuthService implements OnInit {
+  public users: Users[] = [];
+  // private canLog: boolean = false;
+  // showMenuEmitter = new EventEmitter<boolean>();
 
-  showMenuEmitter = new EventEmitter<boolean>();
+  constructor(private router: Router,
+              private userService: UserService) { 
 
-  constructor(private router: Router) { }
+  }
 
-  logIn(user?: Users) {
-    console.log("logado");
-  //   let data = user.;
-  //   let id;
-  //   data.map( (find) => {
-  //     if (user.email === find.email && user.password === find.password) {
-  //       this.canLog = true;
-  //       id = find.id;
-  //     }
-  //   });
-  //   if (this.canLog === true) {
-  //     this.showMenuEmitter.emit(true);
-  //     this.router.navigate(['/dashboard']);
-  //     console.log(id);
+  ngOnInit(): void {
+    this.userService.getAll().subscribe((users: Users[]) => {
+      this.users = users;
+    });
+  }
+  
+  logIn(email: string, password: string) {
+    return this.users.map((users) => {
+      // .find(user => find.email === email && find.password === password);
+      this.users.find(user => {
+        if (user.email === email && user.password === password) {
+          console.log("encontrei");
+          this.router.navigate(['/dashboard']);
+          console.log("Tentei ir para a página");
+          
+          // this.canLog = true;
+        } else {
+          alert("Usuário não encontrado!");
+          console.log("Não encontrei");
+          
+        }
+      })
+
+      // if (this.canLog === true) {
+      //   this.showMenuEmitter.emit(true);
+      //   this.router.navigate(['/dashboard']);
+      // } else {
+      //   this.showMenuEmitter.emit(false);
+      //   alert("Usuário não encontrado!");
+      // }
+    })
+
+    // this.users.map( (find) => {
+    //   if (this.users === find.email && user.password === find.password) {
+    //     this.canLog = true;
+    //     id = find.id;
+    //   }
+    // });
+    // if (this.canLog === true) {
+    //   this.showMenuEmitter.emit(true);
+    //   this.router.navigate(['/dashboard']);
+    //   console.log(id);
       
-  //   } else {
-  //     this.showMenuEmitter.emit(false);
-  //     this.canLog = false;
-  //     user.email = '';
-  //     user.password = '';
-  //     alert('Usuário não encontrado');
-  //   }
-  // }
+    // } else {
+    //   this.showMenuEmitter.emit(false);
+    //   this.canLog = false;
+    //   user.email = '';
+    //   user.password = '';
+    //   alert('Usuário não encontrado');
+    // }
+  }
 
   // logout() {
   //   this.showMenuEmitter.emit(false);
   //   this.canLog = false;
   // }
-  }
 }
